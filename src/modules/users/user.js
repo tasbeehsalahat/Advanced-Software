706913e3bb34ffe934b2bb1c3a23d628ee2069a8
tasbeehsal <s12027639@stu.najah.edu>
2024-02-29 18:11:20 +0300

    showforuser

--- src/modules/users/user.js
+++ src/modules/users/user.js
@@ -1,9 +1,9 @@
-const { updateuser , join } = require('./user.controller.js');
+const { updateuser , join ,shownotification} = require('./user.controller.js');
 const router = require('express').Router();
 const { authenticateJWT } = require('../middleware/middleware.js');
 
 router.put('/user', authenticateJWT,updateuser);
 router.post('/user',authenticateJWT,join);
-router.get('/show',)
+router.get('/show',authenticateJWT,shownotification)
 module.exports = router;
 
