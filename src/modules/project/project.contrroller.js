842d9d4fbf2ff66d4bb4f912fc68cd45b82311ad
tasbeehsal <s12027639@stu.najah.edu>
2024-03-02 03:01:50 +0300

    imagesdoMMe

--- src/modules/project/project.contrroller.js
+++ src/modules/project/project.contrroller.js
@@ -1,27 +1,50 @@
 const connection = require("../../../DB/connection.js");
-const addproject = async function(req, res){
-    const {title, description, level, materials, size, comments, organizer_email, skills} = await req.body;
-    try{     
-        if(req.user.role=='crafter' ){
-            return res.json("you cannot access this page")
-        }
-    const sql = `INSERT INTO project (title,description,level,materials,size,comments,organizer_email,skills) VALUES ('${title}', '${description}', '${level}','${materials}','${size}','${comments}','${organizer_email}','${skills}') `   
-   connection.execute(sql,(err, result) => {
-    if(err){
-       if(err.errno==1452) {
-         
-               return res.json({massege :"this email isn't exsist"});
-        
-       }
-       else return res.json(err);
+const multer = require("multer");
+const path = require("path");
+const express = require('express');const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+        cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
     }
-       return res.json({massege : "You create a project successfully"});
+});
+
+const upload = multer({
+    storage: storage
+}).single('image');
+const addproject = async function (req, res) {
+    try {
+    upload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            return res.json({ error: err.message });
+        } else if (err) {
+            return res.json({ error: err });
+        }
+
+        // File uploaded successfully
+        const { title, description, level, materials, size, comments, organizer_email, skills } = req.body;
+        const image = req.file ? req.file.filename : null; 
+        if (req.user.role === 'crafter') {
+            return res.json("You cannot access this page");
+        }
+
+        const sql = `INSERT INTO project (title, description, level, materials, size, comments, organizer_email, skills, image) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+        connection.execute(sql, [title, description, level, materials, size, comments, organizer_email, skills, image], (err, result) => {
+            if (err) {
+                if (err.errno == 1452) {
+                    return res.json({ message: "This email doesn't exist" });
+                } else {
+                    return res.json(err.stack);
+                }
+            }
+            return res.json({ message: "You created a project successfully" });
+        });
     });
-   }
-   catch(err){
-       return res.json(err);
-  }
-}
+    } catch (err) {
+        return res.json(err.stack);
+    }
+};
+
+
 
 const deleteproject =  function(req, res){
     const { id } = req.body;
@@ -30,20 +53,17 @@ const deleteproject =  function(req, res){
             return res.json("you cannot access this page")
         }
         const sql = `DELETE FROM project WHERE id = '${id}'`;
-         // Execute the SQL query
          connection.execute(sql, (err, result) => {
              if (err) {
-                 return res.json(err); // If an error occurs during deletion
+                 return res.json(err);
              }
-             // If no error, check if any rows were affected
              if (result.affectedRows === 0) {
-                 return res.json("No project found with the provided ID"); // If no project found with provided ID
+                 return res.json("No project found with the provided ID"); 
              }
-             // If successful deletion
              return res.json("Project deleted successfully");
          });
      } catch (err) {
-         return res.json(err); // Catching any unexpected errors
+         return res.json(err); 
      }
  
  }
@@ -64,20 +84,22 @@ const deleteproject =  function(req, res){
         return res.json("Updated successfully");
     });
 }
-
 const getproject = function(req, res) {
-    try {
-        
-        const sql = 'SELECT * FROM project';
+  try {
+      const sql = 'SELECT title, description, level, materials, size, comments, organizer_email, skills, CONCAT("http://", ?, "/upload/", image) AS image_url FROM project';
+      const host = req.headers.host;
 
-        connection.execute(sql, (err, result) => {
-            if (err) {
-                return res.json(err);
-            }
-            return res.json({ projects: result });
-        });
-    } catch (err) {
-        return res.json(err);
-    }
+      connection.execute(sql, [host], (err, result) => {
+          if (err) {
+              return res.json(err);
+          }
+
+          return res.json({ projects: result });
+      });
+  } catch (err) {
+      return res.json(err);
+  }
 };
-module.exports = {addproject, deleteproject,updateproject,getproject };
+
+
+module.exports = {addproject, deleteproject,updateproject,getproject};
