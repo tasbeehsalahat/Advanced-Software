21649760efec18ca9b250d6f1e43f996f85e52df
SewarAslan <s12028585@stu.najah.edu>
2024-03-02 19:41:10 +0200

    chatGPT:external

--- src/modules/HomePage/homeController.js
+++ src/modules/HomePage/homeController.js
@@ -55,7 +55,37 @@ function getItem(req, res) {
     });
   }
 
+  function chatGPT(req, res) {
+    // Request body containing the data to be sent
+    const requestBody = req.body;
+     
+
+  // Options for the external API call
+  const options = {
+      method: 'POST',
+      url: 'https://chatgpt-api8.p.rapidapi.com/',
+      headers: {
+          'content-type': 'application/json',
+          'X-RapidAPI-Key': '0f376ee3a1msh35eb8d384d222e9p136bb8jsncaa1998c71f8',
+          'X-RapidAPI-Host': 'chatgpt-api8.p.rapidapi.com'
+      },
+      body: requestBody,
+      json: true
+  };
+
+  // Make the external API call using the request library
+  request(options, (error, response, body) => {
+      if (error) {
+          console.error('Error sending request:', error);
+          res.status(500).json({ error: 'Internal Server Error' });
+      } else {
+          res.json(body);
+      }
+  });
+}
 module.exports = {
   searchByTerm,
-  getItem
+  getItem,
+  chatGPT
+
 };
