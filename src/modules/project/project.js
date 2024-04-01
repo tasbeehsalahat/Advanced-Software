8e7de0b03b08a6b996c36b42cc76e87c099d4912
shahad-jawabreh <s12028521@stu.najah.edu>
2024-04-01 23:15:46 +0200

    Finish Project

--- src/modules/project/project.js
+++ src/modules/project/project.js
@@ -2,9 +2,9 @@ const { authenticateJWT } = require('../middleware/middleware.js');
 const {addproject,deleteproject,updateproject,getproject,changeProjStatus} = require('./project.contrroller.js');
 const router = require('express').Router();
 
-router.post('/project',authenticateJWT, addproject);
+router.post('/',authenticateJWT, addproject);
 router.delete('/project', authenticateJWT,deleteproject);
-router.patch('/project', authenticateJWT,updateproject);
-router.get('/project',authenticateJWT, getproject);
+router.patch('/manage', authenticateJWT,updateproject);
+router.get('/projects',authenticateJWT, getproject);
 router.patch('/ProjStatus',authenticateJWT,changeProjStatus)
 module.exports = router;
--- src/modules/roles/admin/admin.js
+++ src/modules/roles/admin/admin.js
@@ -3,6 +3,7 @@ const {addCrafter,getCrafter,deactivateUser, selectfeatured,  createEvent} = req
 const {notification,chooseStatus} = require('../../services/notification.js');
 const { updateuser } = require('../users/user.controller.js');
 const { authenticateJWT } = require('../../middleware/middleware.js');
+const { personalInformation } = require('../../services/personalInformation.js');
 const app = express();
 app.post('/crafter',authenticateJWT,addCrafter);
 app.get('/crafters',authenticateJWT,getCrafter);//view users 
@@ -10,6 +11,7 @@ app.patch('/:email',authenticateJWT,updateuser); //update user
 app.put('/status',authenticateJWT,deactivateUser);
 app.get('/notification',authenticateJWT,notification);
 app.post('/chooseStatus',authenticateJWT,chooseStatus);
+app.get('/personalInformation',authenticateJWT,personalInformation);
 app.post('/selectfeatured',authenticateJWT,selectfeatured);
 app.post('/createvent',authenticateJWT,createEvent)
 module.exports= app 
