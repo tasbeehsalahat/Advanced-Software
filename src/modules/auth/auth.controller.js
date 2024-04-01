7ce6c47a5752f60b5ec3a38893b1db4d10c589d3
shahad-jawabreh <s12028521@stu.najah.edu>
2024-04-01 22:36:54 +0200

    Finish Project

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -3,9 +3,7 @@ const connection = require("../../../DB/connection.js");
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 const app = express(); // Creating an instance of Express application
-
 app.use(bodyParser.json());
-
 const { JWT_SECRET_KEY } = require('./../middleware/middleware.js');
 const bcrypt = require('bcrypt');
 const { loginSchema, signupSchema } = require('./../services/validation/validation.js');
@@ -22,30 +20,38 @@ const login = async (req, res) => {
     const sql = 'SELECT * FROM users';
     
     try {
-        connection.execute(sql,async (err, results) => {
+        connection.execute(sql, async (err, results) => {
             if (err) {
                 return res.status(500).json({ message: 'Internal Server Error' });
             }
-            
-            const user = results.find(u => u.email === email && bcrypt.compare(u.password, password));
-
+    
+            const user = await results.find(u => u.email == email);
+    
             if (!user) {
-                return res.status(401).json({ message: 'Invalid email or password' });
+                return res.status(401).json({ message: 'Invalid email' });
             }
+            console.log(password,user.password)
+            const passwordMatch = await bcrypt.compare(password, user.password);
+    
+            if (!passwordMatch) {
+                return res.status(401).json({ message: 'Invalid Password' });
+            }
+    
             if (user.status === 'deactivated') {
                 return res.status(403).json({ message: 'Account is deactivated' });
-              }
-
+            }
+    
             const token = jwt.sign({ email: user.email, role: user.role }, JWT_SECRET_KEY, { expiresIn: '1h' });
     
+            // Check if token exists for user
             const sql2 = `SELECT email_user FROM tokens WHERE email_user = "${email}"`;
-            connection.execute(sql2, [email], (err, results) => {
-                console.log(results);
+            connection.execute(sql2, [email], async (err, results) => {
                 if (err) {
                     return res.status(500).json({ message: 'Internal Server Error' });
                 }
-
+    
                 if (results && results.length > 0) {
+                    // Update token
                     connection.execute(`UPDATE tokens SET token = ? WHERE email_user = ?`, [token, email], (err, result) => {
                         if (err) {
                             return res.json({ message: "Error" });
@@ -53,11 +59,12 @@ const login = async (req, res) => {
                         return res.status(200).json({ message: 'Welcome back', token });
                     });
                 } else {
+                    // Insert new token
                     switch (user.role) {
                         case 'admin':
                         case 'crafter':
                         case 'organizer':
-                            connection.execute(`INSERT INTO tokens (email_user, token) VALUES (?, ?)`, [email, token], (err, result) => {
+                            connection.execute(`INSERT INTO tokens (email_user, token) VALUES ('${email}', '${token}')`, (err, result) => {
                                 if (err) {
                                     return res.status(400).json({ message: "Error" });
                                 }
@@ -73,6 +80,7 @@ const login = async (req, res) => {
     } catch (err) {
         return res.status(500).json({ error: err.stack });
     }
+    
 };
 
 
@@ -86,10 +94,10 @@ const signup = async (req, res) => {
         if (!req.body || Object.keys(req.body).length === 0) {
             return res.status(400).json({ message: 'Request body is missing or empty' });
         }
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        const sql = `INSERT INTO users (email, UserName, password, skills, role, intrests,materials) VALUES (?,?, ?, ?, ?, ?, ?)`;
-        connection.execute(sql, [email, UserName, hashedPassword, skills, role, intrests,materials], (err, result) => {
+        const hashedPassword = await bcrypt.hash(password, 5);
+        console.log(hashedPassword)
+        const sql = `INSERT INTO users (email, UserName, password, skills, role, intrests,materials) VALUES ('${email}','${UserName}','${hashedPassword}', '${skills}', '${role}', '${intrests}', '${materials}')`;
+        connection.execute(sql, (err, result) => {
             if (err) {
                 if (err.errno == 1062) {
                     return res.status(409).json({massege : "Email already exists"});
@@ -111,7 +119,7 @@ const logout = async (req, res) => {
       const token = req.header('Authorization'); 
   console.log(token)
       const sql = `DELETE FROM tokens WHERE token = "${token}"`;
-      connection.execute(sql, [token], (error, results, fields) => {
+      connection.execute(sql, (error, results) => {
         if (error) {
           console.error(error);
           return res.status(500).json({ error: 'An error occurred while logging out' });
